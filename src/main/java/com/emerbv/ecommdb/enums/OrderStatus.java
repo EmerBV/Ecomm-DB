2ec4292f74d348e283e6a30a1b793b17b40a5dd2
EmerBV <emersmc@hotmail.com>
2025-03-31 22:44:36 +0200

    order status fixed

--- src/main/java/com/emerbv/ecommdb/enums/OrderStatus.java
+++ src/main/java/com/emerbv/ecommdb/enums/OrderStatus.java
@@ -1,10 +1,34 @@
 package com.emerbv.ecommdb.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum OrderStatus {
     PENDING,
     PROCESSING,
     PAID,
     SHIPPED,
     DELIVERED,
-    CANCELLED
+    CANCELLED;
+
+    @JsonCreator
+    public static OrderStatus fromString(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        for (OrderStatus status : OrderStatus.values()) {
+            if (status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown order status: " + value);
+    }
+
+    @JsonValue
+    public String getValue() {
+        return this.name();
+    }
 }
